c84e91d5941e57634365e60e8f4cba7f2713395c
lakshmiprasad777 <113926683+lakshmiprasad777@users.noreply.github.com>
2024-02-27 21:40:29 +0530

    Create rl.py

---
+++ rl.py
@@ -0,0 +1,9 @@
+!pip install gym
+!pip install torch
+
+!pip install shimmy>=0.2.1
+
+!pip install tf-agents
+!pip install stable-baselines3 gym
+!pip install stable-baselines
+https://colab.research.google.com/drive/1ln90zvGuL264AJ6-f-tyvWRey6_f9c1-?usp=sharing#scrollTo=TEUonF-k4o7b
